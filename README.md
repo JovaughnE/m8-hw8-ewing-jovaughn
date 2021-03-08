f7f26c04bab1798f60e788488cc4a65acaedaded
Jovaughn E <jovaughn.ewing@ufl.edu>
2021-03-07 19:42:25 -0700

    Final commit.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # m8-hw8-ewing-jovaughn
- 
+ //I followed everything in this one fine with the instruction and study from Module 2.
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// It is always helpful to use comments in your code!
+// Declaration of the variables navLinks and dropdown to define the tools for the following function and for-loop.
+
+var navLinks = document.getElementsByClassName('main-menu')
+var dropdowns = document.getElementsByClassName('dropdown')
+console.log(navLinks)
+
+//Javascript function to toggle the sub-menu to show on hover
+function toggleDropdown() {
+    console.log(this.children[1]);
+    this.children[1].classList.toggle('show');
+    }
+
+//Javascript for-loop to listen for the event of mouse enter and mouse leave to activate the 'Show' CSS toggle.
+for (i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('mouseenter', toggleDropdown);
+    navLinks[i].addEventListener('mouseleave', toggleDropdown);
+}
